41406e176e5a72c7ee130f714403233ba5675fca
Tobias Andersson <tobiasgustaverik@icloud.com>
2020-02-27 13:51:24 +0100

    Help menu features

--- app/src/main/java/com/example/android/studyapp/Help.java
+++ app/src/main/java/com/example/android/studyapp/Help.java
@@ -2,7 +2,12 @@ package com.example.android.studyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Help extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        Button btnPhoneCall = (Button) findViewById(R.id.btnCallUs);
+        Button emailUs = (Button) findViewById(R.id.btnEmailUs);
+
+        btnPhoneCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialContactPhone("0733719883");
+            }
+        });
+
+        emailUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailUs();
+            }
+        });
+    }
+
+    public void dialContactPhone (final String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0733719883"));
+        startActivity(intent);
+    }
+
+    public void emailUs () {
+        try{
+            Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "tobiasgustaverik@icloud.com"));
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+            intent.putExtra(Intent.EXTRA_TEXT, "Text here");
+            startActivity(intent);
+        }catch(ActivityNotFoundException e){
+
+        }
     }
 }
