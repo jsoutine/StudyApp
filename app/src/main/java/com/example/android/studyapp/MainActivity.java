78f5a775176c90f6f5f5562a40d1cbef250b0472
Tobias Andersson <tobiasgustaverik@icloud.com>
2020-02-28 14:33:36 +0100

    Timer Class

--- app/src/main/java/com/example/android/studyapp/MainActivity.java
+++ app/src/main/java/com/example/android/studyapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnHelp = (Button)findViewById(R.id.helpButton);
+
 
     }
 
--- app/src/main/java/com/example/android/studyapp/Tools/Timer/Timer.java
+++ app/src/main/java/com/example/android/studyapp/Tools/Timer/Timer.java
@@ -3,14 +3,14 @@ package com.example.android.studyapp.Tools.Timer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android.studyapp.R;
 
-public class Timer extends AppCompatActivity {
+import java.util.Locale;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
-    }
+public class Timer extends AppCompatActivity {
 }
