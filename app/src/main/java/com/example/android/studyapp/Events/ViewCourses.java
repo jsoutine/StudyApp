f92d0df7f322f2e37457addeb295a2aa2c5d262e
Soutine <jsoutine@gmail.com>
2020-03-16 20:06:52 +0100

    courses almost worked out

--- app/src/main/java/com/example/android/studyapp/Events/ViewCourses.java
+++ app/src/main/java/com/example/android/studyapp/Events/ViewCourses.java
@@ -2,9 +2,10 @@ package com.example.android.studyapp.Events;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,7 +17,10 @@ import java.util.ArrayList;
 
 public class ViewCourses extends AppCompatActivity {
 
-    ArrayList<String> myCourses = new ArrayList<String>();
+    static ArrayList<String> myCourses = new ArrayList<String>();
+    static ArrayAdapter<String> arrayAdapter;
+
+
 
 
     @Override
@@ -25,6 +29,8 @@ public class ViewCourses extends AppCompatActivity {
         setContentView(R.layout.activity_view_events);
 
         final ListView eventListView = findViewById(R.id.eventListView);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, myCourses);
 
         myCourses.add("Maths");
         myCourses.add("Computer Networks");
@@ -37,17 +43,38 @@ public class ViewCourses extends AppCompatActivity {
         myCourses.add("International Relations");
         myCourses.add("Spycraft 201");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, myCourses);
-
         eventListView.setAdapter(arrayAdapter);
 
         eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String courseId = (String) eventListView.getItemAtPosition(i);
-                Intent intent = new Intent(getApplicationContext(),CourseWindow.class);
+                Intent intent = new Intent(getApplicationContext(), CourseNotes.class);
                 intent.putExtra("courseId", courseId);
                 startActivity(intent);
+            }
+        });
+
+       eventListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+
+                new AlertDialog.Builder(ViewCourses.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this course?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                myCourses.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
             }
         });
     }
